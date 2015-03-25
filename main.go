c113e5c74c2174b297bef4ad52b23e996cb7524a
Don Petersen <don@donpetersen.net>
2015-03-25 22:37:41 +0000

    Stub out anticipated commands for earliest version.

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main // import "github.com/CenturyLinkLabs/panamaxcli"
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/codegangsta/cli"
 )
 
@@ -23,15 +23,63 @@ func main() {
 	app.Authors = []cli.Author{{"CenturyLink Labs", "clt-labs-futuretech@centurylink.com"}}
 	app.Commands = []cli.Command{
 		{
-			Name:   "app",
-			Action: handleApp,
+			Name:        "run",
+			Usage:       "Run an application template",
+			Description: "Argument is an application template name.",
+			Action:      noopAction,
+		},
+		{
+			Name:   "status",
+			Usage:  "Get status for a running Panamax instance",
+			Action: noopAction,
+		},
+		{
+			Name:  "app",
+			Usage: "Work with running applications",
+			Subcommands: []cli.Command{
+				{
+					Name:   "list",
+					Usage:  "List applications",
+					Action: appListAction,
+				},
+				{
+					Name:        "describe",
+					Usage:       "Get details for a specific application",
+					Description: "Argument is an application ID.",
+					Action:      noopActionRequiringApp,
+				},
+				{
+					Name:        "logs",
+					Usage:       "View an application's logs",
+					Description: "Argument is an application ID.",
+					Action:      noopActionRequiringApp,
+				},
+			},
+		},
+		{
+			Name:  "service",
+			Usage: "Work with running services",
+			Subcommands: []cli.Command{
+				{
+					Name:        "describe",
+					Usage:       "Get details for a specific service",
+					Description: "Argument is a service ID.",
+					Action:      noopActionRequiringService,
+				},
+				{
+					Name:        "logs",
+					Usage:       "View a service's logs",
+					Description: "Argument is a service ID.",
+					Action:      noopActionRequiringService,
+				},
+			},
 		},
 	}
 
 	app.Run(os.Args)
 }
 
-func handleApp(c *cli.Context) {
+func appListAction(c *cli.Context) {
 	resp, err := http.Get("http://coreos:3001/apps.json")
 	if err != nil {
 		log.Fatalf("Error: %v", err.Error())
@@ -47,4 +95,28 @@ func handleApp(c *cli.Context) {
 	for _, app := range apps {
 		fmt.Printf("App: %d, %s\n", app.ID, app.Name)
 	}
+}
+
+func noopAction(c *cli.Context) {
+	fmt.Println("This command is unimplemented.")
+}
+
+func noopActionRequiringApp(c *cli.Context) {
+	appID := c.Args().First()
+	if appID == "" {
+		log.Fatal("An app is required!")
+	}
+
+	fmt.Println("AppID:", appID)
+	fmt.Println("This command is unimplemented.")
+}
+
+func noopActionRequiringService(c *cli.Context) {
+	serviceID := c.Args().First()
+	if serviceID == "" {
+		log.Fatal("A service is required!")
+	}
+
+	fmt.Println("ServiceID:", serviceID)
+	fmt.Println("This command is unimplemented.")
 }
