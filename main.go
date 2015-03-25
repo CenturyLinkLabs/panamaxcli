8978ffcaee438202b314a6647a06197e15c36721
Chaitanya Akkineni <cakkineni@gmail.com>
2015-03-25 18:50:43 +0000

    Basic app configuration.

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "panamaxcli"
+	app.Version = "0.0.1"
+	app.Authors = []cli.Author{{"CenturyLink Labs", "clt-labs-futuretech@centurylink.com"}}
+	app.Run(os.Args)
+}
