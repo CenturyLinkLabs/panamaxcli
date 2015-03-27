2d7f2242676936e9d1ac2a430cb567c86fa20812
Don Petersen <don@donpetersen.net>
2015-03-27 17:57:04 +0000

    Add Usage to overwrite placeholder help text.

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "panamaxcli"
 	app.Version = "0.0.1"
+	app.Usage = "Panamax command-line utility."
 	app.Authors = []cli.Author{{"CenturyLink Labs", "clt-labs-futuretech@centurylink.com"}}
 	app.Run(os.Args)
 }
